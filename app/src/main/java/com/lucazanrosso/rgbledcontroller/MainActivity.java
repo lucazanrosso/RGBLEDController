b0603e5e3a9314aafeaa652adb18c0a99fbab0d3
lucazanrosso <lucazanrosso94@gmail.com>
2017-04-26 21:52:15 +0200

    Now paired devices are displayed and you can search for other available devices and connect them

--- app/src/main/java/com/lucazanrosso/rgbledcontroller/MainActivity.java
+++ app/src/main/java/com/lucazanrosso/rgbledcontroller/MainActivity.java
@@ -3,10 +3,17 @@ package com.lucazanrosso.rgbledcontroller;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Locale;
+import java.util.Set;
 import java.util.UUID;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -15,6 +22,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
@@ -33,6 +45,13 @@ public class MainActivity extends AppCompatActivity {
 
     private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
+    private ArrayList<String> pairedNames = new ArrayList<>();
+    private ArrayList<String> pairedAddresses = new ArrayList<>();
+    private ListView availableList;
+    private ArrayList<String> availableNames = new ArrayList<>();
+    private ArrayList<String> availableAddresses = new ArrayList<>();
+    LinearLayout RGBLayout;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,10 +65,36 @@ public class MainActivity extends AppCompatActivity {
             if (!mBluetoothAdapter.isEnabled()) {
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-            } else {
-                connect();
             }
         }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        ListView pairedList = (ListView) findViewById(R.id.paired_list);
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                pairedNames.add(device.getName());
+                pairedAddresses.add(device.getAddress());
+            }
+        }
+        pairedList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, pairedNames));
+        pairedList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                connect(pairedNames.get(i), pairedAddresses.get(i));
+            }
+        });
+
+        availableList = (ListView) findViewById(R.id.list_found);
+        availableList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                connect(availableNames.get(i), availableAddresses.get(i));
+            }
+        });
+
+        RGBLayout = (LinearLayout) findViewById(R.id.layout_rgb);
+        RGBLayout.setVisibility(View.GONE);
 
         SeekBar seekBarRed = (SeekBar) findViewById(R.id.seekbar_red);
         seekBarRed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -102,7 +147,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMessage(char c, int i) {
         mConnectedThread.write(String.format(Locale.getDefault(), "%03d", i) + c);
-        System.out.println(String.format(Locale.getDefault(), "%03d", i) + c);
     }
 
     private class ConnectedThread extends Thread {
@@ -180,17 +224,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            connect();
-        }
+//        if (resultCode == RESULT_OK) {
+//            connect();
+//        }
         if (resultCode == RESULT_CANCELED) {
             finish();
         }
     }
 
-    public void connect() {
-        final String ADDRESS = "98:D3:32:20:68:87";
-        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(ADDRESS);
+    public void connect(String name, String address) {
+        if (btSocket != null && btSocket.isConnected()) {
+            RGBLayout.setVisibility(View.GONE);
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+                finish();
+            }
+        }
+
+        Toast.makeText(this, "Connecting with " + name + "...", Toast.LENGTH_SHORT).show();
+
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
         try {
             btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
         } catch (IOException e) {
@@ -202,12 +256,43 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             try {
                 btSocket.close();
+                Toast.makeText(this, "Failed to connect with " + name, Toast.LENGTH_SHORT).show();
             } catch (IOException e2) {
                 finish();
             }
         }
-        mConnectedThread = new ConnectedThread(btSocket);
-        mConnectedThread.start();
+        if (btSocket.isConnected()) {
+            mConnectedThread = new ConnectedThread(btSocket);
+            mConnectedThread.start();
+            RGBLayout.setVisibility(View.VISIBLE);
+            Toast.makeText(this, "Connected with " + name, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                availableNames.add(device.getName());
+                availableAddresses.add(device.getAddress());
+                availableList.setAdapter(new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, availableNames));
+            }
+        }
+    };
+
+    public void searchDevices(View view) {
+        int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+        mBluetoothAdapter.startDiscovery();
+        // Register for broadcasts when a device is discovered.
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
     }
 
     @Override
@@ -221,5 +306,7 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(mReceiver);
     }
 }
